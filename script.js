b14b1fd1a83097559edc3b51339e48ec337530b6
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-04-06 17:43:13 +0200

    modif

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-$(function getCountries() {
-
-  $("#btnShowCountries").click(function () {
-      getCountries();
-  })
+/* $("#btnShowCountries").click(function () {
 
   $.ajax({
-      url: "http://localhost:8000/countries",
+    url: "http://localhost:8000/countries",
 
-      success: function (data, statuts, response) {
+    success: function (data, statuts, response) {
 
-          for (var i = 0; i < data.length; i++) {
-          // for (var i of data) {
+      for (var i = 0; i < data.length; i++) {
+        // for (var i of data) {
 
-              $("#countries").append(<li>${data[i]}</li>);
+        $("#countries").append(<li>${data[i]}</li>);
+
+      }
+      $("text").html("Nouveau Click");
+      console.log(data);
+      // $("#text").html(data)
+    }
+  })
+})
+
+ */
+
+
+function getCountries() {
+
+}
+
+$(function () {
+
+  $("#btnShowCountries").click(function () {
+    $.ajax(
+      "https://localhost:8000/countries",
+      {
+        success: function (data, status, response) {
+
+          for (i = 0; i < data.length; i++) {
+            $("ul").append(<li> ${data[i]} </li>)
 
           }
-          $("text").html("Nouveau Click");
-          console.log(data);
-          // $("#text").html(data)
-      }
-  })
 
+        }
+      }
+    )
+  }
+  )
 })
